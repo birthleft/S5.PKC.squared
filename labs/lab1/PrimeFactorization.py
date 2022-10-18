68a0cda394b8f659ee277681ada431a64ea2fc95
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-10-18 14:43:43 +0300

    add prime factorization algorithm

---
+++ labs/lab1/PrimeFactorization.py
@@ -0,0 +1,29 @@
+from GCDSolver import GCDSolver
+
+
+class PrimeFactorization(GCDSolver):
+    def solve(self, a: int, b: int) -> int:
+        gcd = 1
+        prime_factorization_of_a = self.prime_factor(a)
+        prime_factorization_of_b = self.prime_factor(b)
+
+        for factor in prime_factorization_of_a.keys():
+            if factor in prime_factorization_of_b:
+                gcd *= factor ** min(prime_factorization_of_a[factor], prime_factorization_of_b[factor])
+
+        return gcd
+
+    @staticmethod
+    def prime_factor(number: int) -> dict[int, int]:
+        factors = {}
+        for i in range(2, number + 1):
+            while number % i == 0:
+                if i in factors:
+                    factors[i] += 1
+                else:
+                    factors[i] = 1
+                number //= i
+        return factors
+
+# for i in range(1, 100):
+#     print(f"{i}: {PrimeFactorization.prime_factor(i)}")
