8934d51494b8f08f00b1fe3ca54ed7b3620dea28
TAVI <octavianpatilea@gmail.com>
2022-11-09 23:09:56 +0200

    Updated Miller-Rabin.
    Took 3 hours 37 minutes

--- assignments/Assignment A/miller-rabin.py
+++ assignments/Assignment A/miller-rabin.py
@@ -1,104 +1,170 @@
-"""
-NOTE: it doesn't replace irrelevant values with 'x'.
-
-Also, the Miller-Rabin test is not a deterministic test. It is a probabilistic test.
-"""
-import sys
-from functools import reduce
+import string
+from assignments.utils import Style, Formatting
 
 
-# python get bits from int https://stackoverflow.com/questions/9945720/python-extracting-bits-from-a-byte
 def is_set(number, n):
-    """Return True if the nth bit of number is set."""
-    return number & (1 << n) > 0
+    # We use the binary "and" operator to mask the given number
+    # and see if the Nth bit is set or not (a.k.a. is different than 0).
+    # Reference: https://stackoverflow.com/questions/9945720/python-extracting-bits-from-a-byte
+    return number & (1 << n) != 0
 
 
-def repeated_squaring_modular_exponentiation(base, exponent, modulus):
-    a: int = 1
+def repeated_squaring_modular_exponentiation(base: int, exponent: int, modulus: int):
+    result: int = 1
     if exponent == 0:
-        return a
+        return result
 
-    c: int = base
+    fragment: int = base
+
     if is_set(exponent, 0):
-        a = base
+        result = fragment
+    for bit_position in range(1, exponent.bit_length()):
+        fragment = (fragment * fragment) % modulus
+        if is_set(exponent, bit_position):
+            result = (result * fragment) % modulus
 
-    for i in range(1, exponent.bit_length()):
-        c = (c * c) % modulus
-        if is_set(exponent, i):
-            a = (a * c) % modulus
-
-    return a
+    return result
 
 
-def compute_exponent_of_factor(number: int, factor: int):
+def compute_from_number_exponent_of_factor(number: int, factor: int):
+    # This computes the exponent of a given number's given factor.
+    # 24 = 2^3 * 3 ==> compute_exponent_of_factor_from_number(24, 2) = 2
     exponent = 0
     while number % factor == 0:
         exponent += 1
-        number /= 2
+        number /= factor
     return exponent
 
 
-def number_as_sum_of_powers_of_2(number: int):
-    # return reduce(lambda acc, i: acc + [i] if is_set(n, i) else acc, range(n.bit_length()), [])
-    return reduce(lambda x, y: f"{x} + {y}", [f"2^{i}" for i in range(number.bit_length()) if is_set(number, i)])
+def binary_representation_of_number(number: int):
+    return bin(number)[2:]
 
 
-def compute_and_print_the_relevant_powers_of_2(t, n):
-    print(f"t = {bin(t)[2:]} = {number_as_sum_of_powers_of_2(t)}.", end=' ')
-    print(f"\n\trelevant exponents: {list([i for i in range(t.bit_length()) if is_set(t, i)])}")
-    print()
-    for i in range(0, t.bit_length()):
-        print(f"2^(2^{i}) mod {n} = {(2 ** 2 ** i) % n}", end=' ')
-        print('RELEVANT' if is_set(t, i) else '')
-    print()
+def print_necessary_powers_of_two(modulus: int, t: int):
+    for necessary_power in range(10):
+        end_type: string
+        computed_value: string
+
+        if necessary_power == 4 or necessary_power == 9:
+            end_type = "\n"
+        else:
+            end_type = "\t\t\t"
+
+        if necessary_power < t.bit_length():
+            computed_value = str(2 ** (2 ** necessary_power) % modulus)
+        else:
+            computed_value = "x"
+
+        print("2{} = ".format(Formatting.superscript(f"(2^{necessary_power})"))
+              + Style.YELLOW + f"{computed_value}" + Style.RESET,
+              end=end_type)
 
 
-def miller_rabin(n: int):
-    print(f"n = {n}")
-    if n <= 1 or n % 2 == 0:
-        return False
+def miller_rabin(number: int, expected_sequence_size: int = 5):
+    print("n = " + Style.YELLOW + str(number) + Style.RESET, end="\n\n")
 
+    if number <= 2:
+        print(Style.RED + "ERROR: Condition not met. (" + Style.CYAN + "n >= 3" + Style.RED + ")" + Style.RESET)
+        return
+    elif number % 2 == 0:
+        print(Style.RED + "ERROR: Condition not met. (" + Style.CYAN + "n odd" + Style.RED + ")" + Style.RESET)
+        return
+
+    print("Decomposition:")
+
+    # In order to get s and t, we know that:
     # n - 1 = 2^s * t
-    s: int = compute_exponent_of_factor(n - 1, 2)
-    t: int = (n - 1) >> s  # <=> (n-1)/2^s https://wiki.python.org/moin/BitwiseOperators
-    print(f"n - 1 = 2^{s} * {t}")
+    s: int = compute_from_number_exponent_of_factor(number - 1, 2)
+    print("s = " + Style.YELLOW + str(s) + Style.RESET, end="\t\t\t")
 
-    compute_and_print_the_relevant_powers_of_2(t, n)
+    t: int = (number - 1) >> s
+    # It's similar to (n-1)/2^s.
+    # Reference: https://wiki.python.org/moin/BitwiseOperators
+    print("t = " + Style.YELLOW + str(t) + Style.RESET, end="\t\t\t")
+    print("t (in binary) = " + Style.YELLOW + binary_representation_of_number(t) + Style.RESET, end="\n\n")
 
-    is_prime = True
+    iteration_count = 1
+    is_not_prime_lazy_check: bool = False
     for a in [2, 3, 5]:
+        is_possibly_prime: bool = False
+
+        print("Iteration "
+              + Style.YELLOW + f"k = {iteration_count}" + Style.RESET
+              + " for "
+              + Style.YELLOW + f"a = {a}" + Style.RESET
+              + " (results mod "
+              + Style.YELLOW + "n" + Style.RESET
+              + "):")
+
         sequence = []
-        print(f"a = {a}")
 
         # we need to compute a^t, a^(2t), a^(4t), ..., a^(2^s * t)
         # Step 2 of https://moodle.cs.ubbcluj.ro/pluginfile.php/46227/mod_resource/content/1/pkc-c03.pdf#page=20
-        sequence_size = 5  # normally s + 1, but for the quiz it's 5
-        for r in range(sequence_size):
-            current_sequence_value = repeated_squaring_modular_exponentiation(a, 2 ** r * t, n)
+        if a == 2:
+            print_necessary_powers_of_two(number, t)
+            print()
 
-            print(f"{a}^(2^{r} * {t}) mod {n} = {current_sequence_value}")
-            sequence.append(current_sequence_value)
-        print()
+        # The Sequence Size will ALWAYS BE s + 1.
+        # The quiz contains an expected Sequence Size of 5, but populates the remaining with "x"
+        for r in range(expected_sequence_size):
+            current_sequence_value: string
+            if r < s + 1 and not is_not_prime_lazy_check:
+                current_sequence_value = repeated_squaring_modular_exponentiation(a, 2 ** r * t, number)
+                sequence.append(current_sequence_value)
+            else:
+                current_sequence_value = "x"
+
+            end_type: string
+            if r == expected_sequence_size - 1:
+                end_type = "\n"
+            else:
+                end_type = "\t\t\t"
+
+            print("{}{} = ".format(a, Formatting.superscript(f"2^{r}t"))
+                  + Style.YELLOW + f"{current_sequence_value}" + Style.RESET, end=end_type)
 
         # Step 3 of https://moodle.cs.ubbcluj.ro/pluginfile.php/46227/mod_resource/content/1/pkc-c03.pdf#page=20
-        is_prime = sequence[0] == 1 or n - 1 in sequence[:-1]
+        if not is_not_prime_lazy_check:
+            if sequence[0] == 1:
+                is_possibly_prime = True
+            else:
+                for pair in list(zip(sequence[:-1], sequence[1:])):
+                    if pair[0] == number - 1 and pair[1] == 1:
+                        is_possibly_prime = True
+                        break
+            if not is_possibly_prime:
+                is_not_prime_lazy_check = True
 
-    return is_prime
+        iteration_count += 1
+        print()
+
+    print("Conclusion:", end="\n\n")
+    print("n is prime (yes/no)= " + Style.YELLOW + ("yes" if is_possibly_prime else "no") + Style.RESET)
+    print(Style.MAGENTA + f"Don't trust me? Try a Miller-Rabin calculator https://planetcalc.com/8995/" + Style.RESET,
+          end="\n\n\n")
 
 
-assert repeated_squaring_modular_exponentiation(42, 51, 73) == 17
-
-
-def choose_value():
-    if len(sys.argv) != 2:
-        return 3017
-    else:
-        try:
-            return int(sys.argv[1])
-        except ValueError:
-            raise RuntimeError(f"Usage: {sys.argv[0]} [number]")
+# This Function is used STRICTLY for solving Assignment A on Moodle,
+# taking into account the amount of pages the test contains.
+#
+# You can also simply hardcode the number into the main function.
+def show_ui_exam_version(page_count: int = 2):
+    for page in range(page_count):
+        while True:
+            try:
+                print(f"PAGE {page + 1}/2")
+                user_input = int(input("Use the Miller-Rabin test to decide whether the number is prime or not. "
+                                       "Number: "))
+                miller_rabin(user_input)
+                break
+            except ValueError:
+                print(Style.RED + "ERROR: Input is NOT a number." + Style.RESET, end="\n\n")
+    print(Style.BLUE + "GOOD LUCK!" + Style.RESET)
 
 
 if __name__ == '__main__':
-    print(f"Miller-Rabin (aka: is n prime?): {miller_rabin(choose_value())}")
-    print(f"Don't trust me? Try a Miller-Rabin calculator https://planetcalc.com/8995/")
+    show_ui_exam_version()
+
+    # The hardcoded variants as alternative. Uncomment them and replace them with whatever numbers you have.
+    # miller_rabin(7121)
+    # miller_rabin(1521)
