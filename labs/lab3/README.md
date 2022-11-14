76290d51fd7401f49dbcda0a75550272a0c16226
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-14 16:05:37 +0200

    add pollard ρ algorithm

---
+++ labs/lab3/README.md
@@ -0,0 +1,2 @@
+2. Pollard’s ρ algorithm. The implicit function will be f(x) = x^2 + 1, but it will also allow the use
+of a function f given by the user.
--- utils/fermat-method.py
+++ utils/fermat-method.py
@@ -3,14 +3,15 @@ from typing import Union
 
 
 def fermat_method(n: int, b: int) -> Union[bool, str]:
+    print(f"n = {n}, b = {b}")
     t0 = math.isqrt(n)  # https://docs.python.org/3.8/library/math.html#math.isqrt
+    print(f"t0 = {t0}")
 
     for t in range(t0 + 1, t0 + b + 1):
         t2n = t ** 2 - n
         s_float = math.sqrt(t2n)
         s = int(s_float)
-        print(f"t^2 - n = {t2n}, perfect square (yes/no) = {'yes' if s_float == s else 'no'}")
-        # print(f"t = {t}, s = {s}, s_float = {s_float}")
+        print(f"t = t0 + {t - t0}, t^2 - n = {t2n}, perfect square (yes/no) = {'yes' if s_float == s else 'no'}")
         if s_float == s:
             print(f"s = {s}, t = {t}")
             return f"{t - s} * {t + s}"
@@ -19,4 +20,4 @@ def fermat_method(n: int, b: int) -> Union[bool, str]:
 
 
 if __name__ == '__main__':
-    print(fermat_method(9699, 20))
+    print(fermat_method(6811, 20))
